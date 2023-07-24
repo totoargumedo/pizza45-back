5632bd132042b47d27820c83bb90700412b0e477
msdario12 <mansilladario94@gmail.com>
2023-07-24 20:26:39 -0300

    se crea ruta para obtener un producto, se agrega busqueda por querys in get all products

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,38 +1,67 @@
-const { Product } = require("../models/Product.model");
+const { Product } = require('../models/Product.model');
 
 //agregar producto a DB
 const addProduct = async (req, res, next) => {
-  try {
-    const one = req.body;
-    if (
-      !one.name ||
-      !one.description ||
-      !one.price ||
-      !one.category ||
-      !one.image
-    ) {
-      return res.status(400).json({ success: false, response: "Data missing" });
-    }
-    const newProduct = await Product.create(one);
-    res.status(201).json({ success: true, response: newProduct });
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const one = req.body;
+		if (
+			!one.name ||
+			!one.description ||
+			!one.price ||
+			!one.category ||
+			!one.image
+		) {
+			return res.status(400).json({ success: false, response: 'Data missing' });
+		}
+		const newProduct = await Product.create(one);
+		res.status(201).json({ success: true, response: newProduct });
+	} catch (error) {
+		next(error);
+	}
+};
+
+// traer un producto
+const getOneProduct = async (req, res, next) => {
+	try {
+		const id = req.params.id;
+		const one = await Product.findById(id);
+		if (!one) {
+			return res.status(200).json({
+				success: true,
+				response: 'Product not found',
+			});
+		}
+		return res.status(200).json({ success: true, response: one });
+	} catch (error) {
+		next(error);
+	}
 };
 
 //traer productos cargados
 const getProducts = async (req, res, next) => {
-  try {
-    const all = await Product.find({});
-    if (all.length == 0) {
-      return res
-        .status(200)
-        .json({ success: true, response: "No products in DB" });
-    }
-    return res.status(200).json({ success: true, response: all });
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const { category } = req.query;
+		if (category) {
+			const all = await Product.find({ category: category });
+			if (all.length == 0) {
+				return res.status(200).json({
+					success: true,
+					response: `No products in category ${category} in DB`,
+				});
+			}
+			return res.status(200).json({ success: true, response: all });
+		} else {
+			const all = await Product.find({});
+			if (all.length == 0) {
+				return res
+					.status(200)
+					.json({ success: true, response: 'No products in DB' });
+			}
+			return res.status(200).json({ success: true, response: all });
+		}
+	} catch (error) {
+		next(error);
+	}
 };
 
-module.exports = { addProduct, getProducts };
+module.exports = { addProduct, getProducts, getOneProduct };
--- src/routers/products.js
+++ src/routers/products.js
@@ -1,20 +1,26 @@
-const { Router } = require("express");
-const { addProduct, getProducts } = require("../controllers/products");
+const { Router } = require('express');
+const {
+	addProduct,
+	getProducts,
+	getOneProduct,
+} = require('../controllers/products');
 
 //Routers
 const router_products = Router();
 
 //ENDPOINTS DE PRODUCTO
-router_products.get("/", getProducts);
+router_products.get('/', getProducts);
+// obtener un producto
+router_products.get('/:id', getOneProduct);
 
-router_products.post("/", addProduct);
+router_products.post('/', addProduct);
 
-router_products.put("/", (req, res) => {
-  res.json({ success: true, response: "productos" });
+router_products.put('/', (req, res) => {
+	res.json({ success: true, response: 'productos' });
 });
 
-router_products.delete("/", (req, res) => {
-  res.json({ success: true, response: "productos" });
+router_products.delete('/', (req, res) => {
+	res.json({ success: true, response: 'productos' });
 });
 
 module.exports = { router_products };
